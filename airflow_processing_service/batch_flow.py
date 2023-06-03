7d3d02cdf5dd6608b9c1679d6b7a8c6826d08533
WagnoLeaoSergio <wagnoleao@gmail.com>
2023-06-03 03:04:47 -0300

    tarefa de publicacao implementada

--- airflow_processing_service/batch_flow.py
+++ airflow_processing_service/batch_flow.py
@@ -1,6 +1,8 @@
 import os
 import json
 import requests
+import pandas as pd
+from dataclasses import dataclass
 from datetime import datetime, timedelta
 
 # The DAG object; we'll need this to instantiate a DAG
@@ -10,17 +12,29 @@ from airflow.decorators import task
 DATALAKE_HOST = "127.0.0.1"
 DATALAKE_PORT = "5000"
 
+CLOUD_HOST = "127.0.0.1"
+CLOUD_PORT = "8000"
 
-def build_batch_workflow_dag_v0(user_id: str):
+
+@dataclass
+class User:
+    id: str
+    email: str
+
+
+def build_batch_workflow_dag_v0(user: User):
+
+    user_id = user.id
+    user_email = user.email
 
     with DAG(
         f"batch_flow_{user_id}",
-        schedule=timedelta(minutes=10),
+        schedule=timedelta(minutes=15),
         start_date=datetime.now() + timedelta(minutes=1),
         max_active_runs=1,
         default_args={
             "depends_on_past": False,
-            "email": ["wagnoleao@gmail.com"],
+            "email": "wagnoleao@gmail.com",
             "retries": 1,
             "retry_delay": timedelta(minutes=5),
             "max_active_tasks": 1,
@@ -36,30 +50,81 @@ def build_batch_workflow_dag_v0(user_id: str):
         #     return open(os.path.join("temp_data", "health_data.csv"), "r")
 
         @task(task_id="run_model_training")
-        def make_post_request(user_id) -> dict:
-            print(user_id)
+        def make_post_request(user_id: str):
             health_file = open(os.path.join(
                 "temp_data", "health_data.csv"), "rb")
-            print(health_file)
             predictions = requests.post(
                 f"http://{DATALAKE_HOST}:{DATALAKE_PORT}/api/v1/mlflow/{user_id}",
                 files={"health_file": health_file}
             ).json()
 
-            print(predictions)
+            health_file.close()
             return predictions
 
-        @task(task_id="save_predictions")
-        def save_predictions(user_id: str, predictions: dict) -> None:
-            with open(os.path.join("temp_data", f"{user_id}_predictions.json"), "w") as out:
-                json.dump(predictions, out)
+        @task(task_id="process_anomalies")
+        def process_anomalies(predictions):
+
+            health_data = pd.read_csv(
+                os.path.join(
+                    "temp_data",
+                    "health_data.csv"
+                ),
+                index_col=[0]
+            )
+            health_data = health_data.reset_index(drop=True)
+            health_data.set_index("date", inplace=True)
+            health_data.sort_index(inplace=True)
+
+            preds_datafile = {"date": [], "heart_rate_pred": []}
+
+            for pred in predictions['predictions']:
+                preds_datafile['date'].append(pred[0])
+                preds_datafile['heart_rate_pred'].append(pred[1])
+
+            predictions = pd.DataFrame(preds_datafile)
+
+            predictions.set_index("date", inplace=True)
+            predictions.sort_index(inplace=True)
+
+            divergences = []
+
+            for index, pred in predictions.iterrows():
+                diff = abs(health_data.loc[index, "heart"] - pred.values[0])
+                diff = diff / health_data.loc[index, "heart"]
+                if diff > 0.25:
+                    divergences.append((index, diff))
+
+            return divergences
+
+        @task(task_id="process_anomalies")
+        def send_results(divergences):
+            data = []
+            for div in divergences:
+                data.append(
+                    {
+                        "id": user.id,
+                        "email": user_email,
+                        "date": div[0],
+                        "message": """
+                                It was detected that your heart rate
+                                is instable at this time of the day.
+                                """
+                    }
+                )
+
+            response = requests.post(
+                f"http://{CLOUD_HOST}:{CLOUD_PORT}/notification",
+                json={"notifications": data}
+            ).json()
+
             return None
 
-        save_predictions(user_id, make_post_request(user_id))
+        send_results(process_anomalies(make_post_request(user_id)))
         return dag
 
 
-users_list = ["user1"]
+users_list = [("user1", "wagnoleao@gmal.com")]
 
 for user in users_list:
-    globals()[f"batch_flow_{user}"] = build_batch_workflow_dag_v0(user)
+    globals()[f"batch_flow_{user[0]}"] = build_batch_workflow_dag_v0(
+        User(user[0], user[1]))
